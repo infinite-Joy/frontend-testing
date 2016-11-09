458fb67c903b5bd082ba02005b84ac2a78d96f8d
Joydeep BHattacharjee <joydeepubuntu@gmail.com>
2016-11-09 17:40:12 +0000

    checkout all buttons are working

--- homepage.py
+++ homepage.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-import sys
-import os
 import time
 import logging
 import datetime
-from random import randint
+from urllib.request import urlopen, HTTPError, URLError
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,6 +10,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+URL = "https://flawcode.com"
+PATH = "/home/joy/github/frontend-testing/phantomjs"
 
 
 def get_date():
@@ -33,20 +35,21 @@ fileHandler = logging.FileHandler("{0}.log".format(fileName))
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 
-logger.setLevel(logging.DEBUG)
-
+# logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # this is to implement the singleton
 # we do not want multiple instances of browser being implemented
 class SingletonMetaClass(type):
-    def __init__(cls,name,bases,dict):
-        super(SingletonMetaClass,cls)\
-          .__init__(name,bases,dict)
+    def __init__(cls, name, bases, dict):
+        super(SingletonMetaClass, cls)\
+          .__init__(name, bases, dict)
         original_new = cls.__new__
-        def my_new(cls,*args,**kwds):
-            if cls.instance == None:
+
+        def my_new(cls, *args, **kwds):
+            if cls.instance is None:
                 cls.instance = \
-                  original_new(cls,*args,**kwds)
+                  original_new(cls, *args, **kwds)
             return cls.instance
         cls.instance = None
         cls.__new__ = staticmethod(my_new)
@@ -64,6 +67,7 @@ class Browser(object):
 
     """
     __metaclass__ = SingletonMetaClass
+
     def __init__(self, browser_type):
         super(Browser, self).__init__()
         self.browser_type = browser_type
@@ -73,7 +77,7 @@ class Browser(object):
             self.browser.set_window_size(1120, 550)
 
         elif self.browser_type == "PhantomJS":
-            self.browser = webdriver.PhantomJS()
+            self.browser = webdriver.PhantomJS(PATH)
             self.browser.set_window_size(1120, 550)
 
         # for logging
@@ -84,31 +88,36 @@ class Browser(object):
     def __enter__(self):
         return self
 
-    def hit_url(self):
+    def hit_url(self, the_url):
         self.browser.get(the_url)
         self.logger.info("browser url: %s" % self.browser.current_url)
 
-    def see_recomendations(self):
-        WebDriverWait(self.browser, 50).until(
-            EC.visibility_of_element_located((By.ID, "ide")))
+    def get_all_buttons(self):
+        try:
+            for i in range(1, 10):
+                yield self.browser.find_element(By.XPATH,
+                                             ".//*[@id='app']/div[1]/a[%s]" % i)
+        except:
+            pass
 
-        self.logger.info("browser url: %s" % self.browser.current_url)
-
-        # find the recomendations
-        for element in self.browser.find_elements_by_id("ide"):
-            self.logger.info("element text: %s" % element.text)
-            self.logger.info("element tag name: %s" % element.tag_name)
-            self.logger.info("element parent: %s" % element.parent)
-            self.logger.info("element location: %s" % element.location)
-            self.logger.info("element size: %s" % element.size)
-
-        time.sleep(10)
+    def validate_url(self, url):
+        self.logger.info("To test the url %s" % url)
+        try:
+            r = urlopen(url)
+            return True
+        except HTTPError as e:
+                print(e.code)
+        except URLError as e:
+                print(e.args)
 
     def execute_steps(self):
-        self.hit_url()
-        self.fill_values()
-        self.select_role()
-        self.see_recomendations()
+        url = URL
+        self.hit_url(url)
+        for a in self.get_all_buttons():
+            pre_validated_url = a.get_attribute('href')
+            if not self.validate_url(pre_validated_url):
+                self.logger.debug("the url %s is not working"
+                                  % pre_validated_url)
 
     def __exit__(self, *args):
         self.browser.quit()
@@ -118,13 +127,12 @@ class Browser(object):
 
 def main():
     try:
-        #with Browser("PhantomJS") as b:
+        # with Browser("PhantomJS") as b:
         with Browser("PhantomJS") as b:
             b.execute_steps()
-    except Exception, e:
+    except Exception as e:
         logger.debug("Error got: %s" % e)
         raise e
-
 
 
 if __name__ == '__main__':
